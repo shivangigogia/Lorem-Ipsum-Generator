7422e7a5115acd60a4232135babb159ed5aa8b1a
shivangigogia <shivangigogia20799>
2018-09-06 00:02:58 +0530

    first commit

---
+++ scripts/ShareComponent.js
@@ -0,0 +1,16 @@
+import pubSub from './pubSub';
+
+export default function() {
+  const twitterBtn = document.getElementById('twitter-share');
+  
+  function addTwitterLink({haikuTitle, haiku}) {
+    const url = "http://ramenhog.com/haiku4u";
+    const regex = /\s<br\s*[\/]?>\s/gi;
+		const encodedHaiku = haiku.replace(regex, "%0A");
+    const shareUrl = `https://twitter.com/intent/tweet?url=${url}&text=${haikuTitle}%0A${encodedHaiku}%0A-`;
+
+    twitterBtn.setAttribute('href', shareUrl);
+  }
+  
+  pubSub.subscribe('generatedHaiku', addTwitterLink);
+}
